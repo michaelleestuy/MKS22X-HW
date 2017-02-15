b6179522c492ce7cb7f69aded57fb9fb17ba7243
Michael Lee <michaelleestuy@gmail.com>
2017-02-15 14:45:08 -0500

    completed

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -1,16 +1,88 @@
 public class KnightBoard{
     private int[][] board;
+    private boolean solved;
+
+    private int[] r = {1, 1, -1, -1, 2, 2, -2, -2};
+    private int[] c = {2, -2, 2, -2, 1, -1, 1, -1};
+    // r = {1, 1, -1, -1, 2, 2, -2, -2};
+    // c = {2, -2, 2, -2, 1, -1, 1, -1};
 
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
+	solved = false;
     }
 
-    public 
+    public String toString(){
+	String a = "";
+
+	for(int i = 0; i < board.length; i++){
+	    for(int j = 0; j < board[0].length; j++){
+		if(board[i][j] < 10){
+		    a += "" + " " + board[i][j] + " ";
+		}
+		else{
+		    a += "" + board[i][j] + " ";
+		}
+	    }
+	    a += "\n";
+	}
+
+	return a;
+    }
+
+    public void solve(){
+	for(int i = 0; i < board.length; i++){
+	    for(int k = 0; k < board.length; k++){
+		board[i][k] = 1;
+		solveH(i, k, 2);
+		if(!solved){
+		    board[i][k] = 0;
+		}
+		if(solved){
+		    return;
+		}
+	    }
+	}
+    }
+
+    public void solveH(int row, int col, int level){
+	
+	if(solved){
+	    return;
+	}
 
 
+	if(level > board.length * board[row].length){
+	    solved = true;
+	    return;
+	}
+	
+
+	
+	for(int i = 0; i < 8 && !solved; i++){
+	    if(row + r[i] >= 0 && row + r[i] < board.length &&
+	       col + c[i] >= 0 && col + c[i] < board[row].length){
+		if(board[row + r[i]][col + c[i]] == 0){
+		    board[row + r[i]][col + c[i]] = level;
+		    //	    attempt = true;
+		    solveH(row + r[i], col + c[i], level + 1);
+		    
+		    if(!solved){
+			board[row + r[i]][col + c[i]] = 0;
+		    }
+		}
+	    }
+	}
+
+    }
 
 
+    public static void main(String[]args){
+	KnightBoard a = new KnightBoard(5, 5);
+	a.solve();
+	System.out.println(a);
 
+    }
 
 
 
