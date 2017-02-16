e4c2c2c30a1d9bacda6ba18393a2ad0c104fb1e3
Michael Lee <michaelleestuy@gmail.com>
2017-02-16 14:46:07 -0500

    almostplus

--- 03KnightBoard/KnightBoard.java
+++ 03KnightBoard/KnightBoard.java
@@ -4,12 +4,29 @@ public class KnightBoard{
 
     private int[] r = {1, 1, -1, -1, 2, 2, -2, -2};
     private int[] c = {2, -2, 2, -2, 1, -1, 1, -1};
-    // r = {1, 1, -1, -1, 2, 2, -2, -2};
-    // c = {2, -2, 2, -2, 1, -1, 1, -1};
+
+    private int[][] moves;
 
     public KnightBoard(int startingRows, int startingCols){
 	board = new int[startingRows][startingCols];
 	solved = false;
+	moves = new int[startingRows][startingCols];
+	for(int i = 0; i < startingRows; i++){
+	    for(int k = 0; k < startingCols; k++){
+		setup(i, k);
+	    }
+	}
+    }
+
+    private void setup(int row, int col){
+	int asdf = 0;
+	for(int i = 0; i < 8; i++){
+	    if(row + r[i] >= 0 && row + r[i] < board.length &&
+	       col + c[i] >= 0 && col + c[i] < board[row].length){
+		asdf += 1;
+	    }
+	}
+	moves[row][col] = asdf;
     }
 
     public String toString(){
@@ -28,6 +45,37 @@ public class KnightBoard{
 	}
 
 	return a;
+    }
+
+    public void solveHplus(int row, int col, int level){
+	if(solved){
+	    return;
+	}
+
+	if(level > board.length * board[row].length){
+	    solved = true;
+	    return;
+	}
+
+	if(moves[row][col] == 0){
+	    return;
+	}
+
+	int[]movee = new int[8];
+
+
+	for(int i = 0; i < 8 && !solved; i++){
+	    if(row + r[i] >= 0 && row + r[i] < board.length &&
+	       col + c[i] >= 0 && col + c[i] < board[row].length){
+	        movee[i] = moves[row + r[i]][col + c[i]];
+	    }
+	    else{
+		movee[i] = -1;
+	    }
+	}
+	
+
+
     }
 
     public void solve(){
@@ -78,7 +126,7 @@ public class KnightBoard{
 
 
     public static void main(String[]args){
-	KnightBoard a = new KnightBoard(5, 5);
+	KnightBoard a = new KnightBoard(5, 6);
 	a.solve();
 	System.out.println(a);
 
