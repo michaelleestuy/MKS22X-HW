b567f14cc589acbaac8b855495bb52aa6b0f690a
Michael Lee <michaelleestuy@gmail.com>
2017-02-28 14:42:53 -0500

    wip

--- 04Maze/Maze.java
+++ 04Maze/Maze.java
@@ -5,6 +5,7 @@ public class Maze{
     
     private char[][]maze;
     private boolean animate;
+    private int sr, sc, er, ec;
 
     public Maze(String file) throws FileNotFoundException{	
 	File infile = new File(file);
@@ -20,16 +21,48 @@ public class Maze{
 	   text.indexOf("S") == -1 ||
 	   text.indexOf("E") != text.lastIndexOf("E") ||
 	   text.indexOf("E") == -1){
-	    throw new FileNotFoundException("no s e");
+	    throw new FileNotFoundException("No S or E found in file");
 	}
 	int width = text.substring(0, text.indexOf("\n")).length();
 	char[][] m = new char[lines][width];
 	for(int i = 0; i < lines; i++){
 	    for(int k = 0; k < width; k++){
-		m[i][k] = text.charAt(i * (width + 1) + k);	
+		m[i][k] = text.charAt(i * (width + 1) + k);
+		if(text.charAt(i * (width + 1) + k) == 'S'){
+		    sr = i; sc = k;
+		}
+		if(text.charAt(i * (width + 1) + k) == 'E'){
+		    er = i; ec = k;
+		}
 	    }
 	}
 	maze = m;
+    }
+
+    private char whatIs(int r, int c, int dir){
+	if(dir == 0)
+	    return maze[r - 1][c];
+	if(dir == 1)
+	    return maze[r][c + 1];
+	if(dir == 2)
+	    return maze[r + 1][c];
+	if(dir == 3)
+	    return maze[r][c - 1];
+	else
+	    return '?';
+    }
+
+    private void solveH(int r, int c){
+	ArrayList<Integer> vs = new ArrayList<Integer>();
+	for(int i = 0; i < 4; i++){
+	    if (whatIs(r, c, i) == ' ')
+		vs.add(i);
+	}
+
+	if(vs.size() == 0){
+	    revert(r, c);
+	    return; 
+	}
     }
 
     public void display(){
@@ -43,7 +76,7 @@ public class Maze{
     }
 
     public static void main(String[]args) throws FileNotFoundException{
-	Maze f = new Maze("data21.dat");
+	Maze f = new Maze("data1.dat");
 	f.display();
     }
 }
