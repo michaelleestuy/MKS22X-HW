54febe7321fc107763d01997d3531bd0145ff14b
Michael Lee <michaelleestuy@gmail.com>
2017-03-07 14:45:05 -0500

    finish makelake

--- USACO/Digger.java
+++ USACO/Digger.java
@@ -7,7 +7,7 @@ public class Digger{
     private String instructions;
     private int n;
 
-    public Digger(String file){
+    public Digger(String file) throws FileNotFoundException{
 	File f = new File(file);
 
 	Scanner in = new Scanner(f);
@@ -32,21 +32,73 @@ public class Digger{
 	    nn += in.nextInt() + " ";
 	}
 
+	instructions = nn;
+
+    }
+
+    public int solve(){
+	Scanner s = new Scanner(instructions);
+
+
+	while(s.hasNextInt()){
+	    stomp(s.nextInt(), s.nextInt(), s.nextInt());
+	}
+	
+	int vol = 0;
+	for(int i = 0; i < elevations.length; i++){
+	    for(int k = 0; k < elevations[i].length; k++){
+		if(elevations[i][k] < elevation){
+		    vol += elevation - elevations[i][k];   
+		}
+	    }
+	}
+
+	return vol * 72 * 72;
+
     }
 
     private void stomp(int r, int c, int press){
 	int target = findHighest(r,c);
 	int row0 = r - 1;
 	int col0 = c - 1;
+	target -= press;
+
+	if(elevations[row0][col0] > target){
+	    elevations[row0][col0] = target;
+	}
+	if (elevations[row0+1][col0] > target){
+	    elevations[row0+1][col0] = target;
+	}
+	if (elevations[row0+2][col0] > target){
+	    elevations[row0+2][col0] = target;
+	}
+	if (elevations[row0][col0+1] > target){
+	    elevations[row0][col0+1] = target;
+	}
+	if (elevations[row0][col0+2] > target){
+	    elevations[row0][col0+2] = target;
+	}
+	if (elevations[row0+1][col0+1] > target){
+	    elevations[row0+1][col0+1] = target;
+	}
+	if (elevations[row0+2][col0+2] > target){
+	    elevations[row0+2][col0+2] = target;
+	}
+	if (elevations[row0+2][col0+1] > target){
+	    elevations[row0+2][col0+1] = target;
+	}
+	if (elevations[row0+1][col0+2] > target){
+	    elevations[row0+1][col0+2] = target;
+	}	
     }
 
     private int findHighest(int r, int c){
 	int highest = 0;
 	int row0 = r - 1;
 	int col0 = c - 1;
-        if (elevations[row0][col0] > highest){
-	    highest = elevations[row0][col0];
-	}
+	// if (elevations[row0][col0] > highest){
+	highest = elevations[row0][col0];
+	//}
 	if (elevations[row0+1][col0] > highest){
 	    highest = elevations[row0+1][col0];
 	}
@@ -73,5 +125,8 @@ public class Digger{
 	}
 	return highest;
     }
+
+
+
 
 }
