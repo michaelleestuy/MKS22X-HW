63d67b5f25131c7a3d695c55cdb4ac435918a6ea
Michael Lee <michaelleestuy@gmail.com>
2017-03-19 20:47:18 -0400

    working quicksort

--- 07QuickSelect/Quick.java
+++ 07QuickSelect/Quick.java
@@ -1,4 +1,24 @@
+import java.util.*;
+
 public class Quick{
+
+    public static void quicksort(int[]data){
+	qh(data, 0, data.length);	
+    }
+
+    private static void qh(int[]data, int start, int end){
+	if( (end - start) <= 1){
+	    return;
+	}
+
+	int a = Partition.part(data, start, end);
+	qh(data, start, a);
+	qh(data, a + 1, end);	
+    }
+
+
+
+    
     public static int quickselect(int[]data, int k){
 	int start = 0;
 	int end = data.length;
@@ -17,7 +37,8 @@ public class Quick{
     }
 
     public static void main(String[]args){
-	int[]a = {0, 1, 2, 4, 6, 8, 5, 3, 4};
-	System.out.println(quickselect(a, 6));
+	int[]a = {2, 4, 63, 41, 44, 192, 9182, 1};
+	quicksort(a);
+	Partition.display(a);
     }
 }
