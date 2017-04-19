e8505e22e4a0111b1ecc443569b2bd645fc1054c
Michael Lee <michaelleestuy@gmail.com>
2017-04-19 14:48:30 -0400

    instance vars

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,3 +1,19 @@
 public class MyDeque{
+    private String[] deck;
+    private int start;
+    private int end;
 
+    public MyDeque(){
+	deck = new String[16];
+	start = 0;
+	end = 0;
+    }
+    
+    private void expand(){
+	if(start - 1 == end){
+	    String[]deck2 = new String[deck.length * 2];
+	    
+	}
+    }
+    
 }
